141a1e755c1d44f7e147b79182bd17c5fa04bd13
Pandaa <TuePVM@gmail.com>
2024-03-04 12:31:52 +0100

    Testtt

--- src/app/Components/datagrid/datagrid.component.ts
+++ src/app/Components/datagrid/datagrid.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 
+// Material
+import { MatTableModule } from '@angular/material/table'
+
 @Component({
   selector: 'app-datagrid',
   standalone: true,
-  imports: [],
+  imports: [
+    MatTableModule,
+  ],
   templateUrl: './datagrid.component.html',
   styleUrl: './datagrid.component.scss'
 })
--- src/app/Components/file-upload/file-upload.component.ts
+++ src/app/Components/file-upload/file-upload.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { log } from 'console';
 
 @Component({
   selector: 'app-file-upload',
@@ -8,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrl: './file-upload.component.scss'
 })
 export class FileUploadComponent {
+
+  file!: File;
+
+  onFilechange(event: any) {
+    console.log(event.target.files[0])
+    this.file = event.target.files[0]
+  }
+  
+  upload() {
+    if (this.file) {
+      console.log(this.file);
+      // this.uploadService.uploadfile(this.file).subscribe(resp => {
+      //   alert("Uploaded")
+      // })
+    } else {
+      alert("Please select a file first")
+    }
+  }
 
 }
